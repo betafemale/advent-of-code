98f7bc8f77da8a1dcc3736ce95e2218c99209739
Li <li.stina.strandberg@gmail.com>
2022-12-03 00:07:39 +0100

    refactor(Day1): use explode and reduce nesting

--- 2022/day1/Day1.php
+++ 2022/day1/Day1.php
@@ -1,27 +1,28 @@
 <?php
 
-$lines = file('Input.txt');
+$file = file_get_contents('Input.txt');
+$elves = explode("\n\n", $file);
 $mostCalories = 0;
 $secondMostCalories = 0;
 $thirdMostCalories = 0;
 $sum = 0;
 
-foreach ($lines as $line) {
-    if (empty(trim($line))) {
-        if ($sum > $mostCalories) {
-            $thirdMostCalories = $secondMostCalories;
-            $secondMostCalories = $mostCalories;
-            $mostCalories = $sum;
-        } elseif ($sum > $secondMostCalories) {
-            $thirdMostCalories = $secondMostCalories;
-            $secondMostCalories = $sum;
-        } elseif ($sum > $thirdMostCalories) {
-            $thirdMostCalories = $sum;
-        }
-        $sum = 0;
-    } else {
-        $sum = $sum + intval($line);
+foreach ($elves as $elf) {
+    $calories = explode("\n", $elf);
+    foreach ($calories as $cal) {
+        $sum = $sum + intval($cal);
     }
+    if ($sum > $mostCalories) {
+        $thirdMostCalories = $secondMostCalories;
+        $secondMostCalories = $mostCalories;
+        $mostCalories = $sum;
+    } elseif ($sum > $secondMostCalories) {
+        $thirdMostCalories = $secondMostCalories;
+        $secondMostCalories = $sum;
+    } elseif ($sum > $thirdMostCalories) {
+        $thirdMostCalories = $sum;
+    }
+    $sum = 0;
 }
 
 $topThree = $mostCalories + $secondMostCalories + $thirdMostCalories;
